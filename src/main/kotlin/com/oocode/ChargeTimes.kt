ac8125d8494eee68173e4e21a3967a3e856ff176
Ivan Moore <ivan@teamoptimization.com>
2024-02-01 15:29:00 +0000

    extracted NationalGridEsoDataProvider ready for unit tests for that

--- src/main/kotlin/com/oocode/ChargeTimes.kt
+++ src/main/kotlin/com/oocode/ChargeTimes.kt
@@ -1,49 +1,31 @@
 package com.oocode
 
-import com.opencsv.CSVReader
-import okhttp3.OkHttpClient
-import org.http4k.client.OkHttp
 import org.http4k.core.HttpHandler
-import org.http4k.core.Method
-import org.http4k.core.Request
-import java.io.StringReader
-import java.time.LocalDateTime
-import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
 
-private const val nationalGridEsoDataUrl =
-    "https://api.nationalgrideso.com/dataset/91c0c70e-0ef5-4116-b6fa-7ad084b5e0e8/resource/db6c038f-98af-4570-ab60-24d71ebd0ae5/download/embedded-forecast.csv"
-
 fun main() {
-    ChargeTimes(httpHandler(), nationalGridEsoDataUrl).printReport()
+    ChargeTimes(NationalGridEsoDataProvider()).printReport()
 }
 
-class ChargeTimes(private val httpHandler: HttpHandler = httpHandler(),
-                  private val url: String = nationalGridEsoDataUrl) {
+data class Row(val dateTimeGmt: ZonedDateTime, val embeddedWindForecast: Int)
+
+class ChargeTimes(
+    private val nationalGridEsoDataProvider: NationalGridEsoDataProvider = NationalGridEsoDataProvider()
+) {
+    constructor(url: String) : this(NationalGridEsoDataProvider(url = url))
+    constructor(httpHandler: HttpHandler) : this(NationalGridEsoDataProvider(httpHandler))
+
     fun printReport() {
         println(report())
     }
 
     fun report(): String {
-        val contents = httpHandler(Request(Method.GET, url)).bodyString()
-        val lines = CSVReader(StringReader(contents)).readAll().toList()
-        val report = "Best times to plug in:\n" + lines.drop(1)
-            .sortedByDescending { it[4].toInt() }
+        return "Best times to plug in:\n" + nationalGridEsoDataProvider.rows().asSequence()
+            .sortedByDescending { it.embeddedWindForecast }
             .take(3)
-            .map { row ->
-                ZonedDateTime.of(
-                    LocalDateTime.parse(row[0])
-                        .withHour(row[1].split(":")[0].toInt())
-                        .withMinute(row[1].split(":")[1].toInt()),
-                    ZoneId.of("GMT")
-                )
-            }
-            .sorted()
-            .map { it.format(RFC_1123_DATE_TIME) }
+            .sortedBy { it.dateTimeGmt }
+            .map { it.dateTimeGmt.format(RFC_1123_DATE_TIME) }
             .joinToString("\n")
-        return report
     }
 }
-
-private fun httpHandler() = OkHttp(OkHttpClient.Builder().followRedirects(true).build())
