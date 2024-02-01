1f2feb3c7aeb2cb140fbbe553de8ab0e26aeabd0
Ivan Moore <ivan@teamoptimization.com>
2024-02-01 11:34:47 +0000

    added ExampleE2e_a_Test

--- src/main/kotlin/com/oocode/ChargeTimes.kt
+++ src/main/kotlin/com/oocode/ChargeTimes.kt
@@ -15,21 +15,27 @@ private const val nationalGridEsoDataUrl =
     "https://api.nationalgrideso.com/dataset/91c0c70e-0ef5-4116-b6fa-7ad084b5e0e8/resource/db6c038f-98af-4570-ab60-24d71ebd0ae5/download/embedded-forecast.csv"
 
 fun main() {
-    val httpClient = OkHttp(OkHttpClient.Builder().followRedirects(true).build())
-    val contents = httpClient(Request(Method.GET, nationalGridEsoDataUrl)).bodyString()
-    val lines = CSVReader(StringReader(contents)).readAll().toList()
-    println("Best times to plug in:\n" + lines.drop(1)
-        .sortedByDescending { it[4].toInt() }
-        .take(3)
-        .map { row ->
-            ZonedDateTime.of(
-                LocalDateTime.parse(row[0])
-                    .withHour(row[1].split(":")[0].toInt())
-                    .withMinute(row[1].split(":")[1].toInt()),
-                ZoneId.of("GMT")
-            )
-        }
-        .sorted()
-        .map { it.format(RFC_1123_DATE_TIME) }
-        .joinToString("\n"))
+    ChargeTimes(nationalGridEsoDataUrl).printReport()
+}
+
+class ChargeTimes(private val url: String) {
+    fun printReport() {
+        val httpClient = OkHttp(OkHttpClient.Builder().followRedirects(true).build())
+        val contents = httpClient(Request(Method.GET, url)).bodyString()
+        val lines = CSVReader(StringReader(contents)).readAll().toList()
+        println("Best times to plug in:\n" + lines.drop(1)
+            .sortedByDescending { it[4].toInt() }
+            .take(3)
+            .map { row ->
+                ZonedDateTime.of(
+                    LocalDateTime.parse(row[0])
+                        .withHour(row[1].split(":")[0].toInt())
+                        .withMinute(row[1].split(":")[1].toInt()),
+                    ZoneId.of("GMT")
+                )
+            }
+            .sorted()
+            .map { it.format(RFC_1123_DATE_TIME) }
+            .joinToString("\n"))
+    }
 }
